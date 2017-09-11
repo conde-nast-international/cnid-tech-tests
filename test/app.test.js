730060b16581c4bee9832ede0dabc1dfcd6fd941
Jake Mulley <mulleyjb@parliament.uk>
2017-09-11 12:57:09 +0100

    option1 - submit first pass of solution

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,29 @@
-// If you prefer another flavour of testing library, please feel free to use it.
+const request = require('supertest');
+const express = require('express');
+const app = require('../app');
 
-describe('app', () => {
-  it('should run the tests', () => {
-    expect(1).toBe(1);
+describe('GET /', function() {
+  it('respond with 200', function(done) {
+    request(app)
+      .get('/')
+      .set('Accept', 'text/html')
+      .expect(200, done);
   });
-})
+});
+
+describe('GET /article/0', function() {
+  it('respond with 200', function(done) {
+    request(app)
+      .get('/article/0')
+      .set('Accept', 'text/html')
+      .expect(200)
+      .expect(validContent)
+      .end(done);
+  });
+});
+
+function validContent(res) {
+  if(!res.text.includes('Lorem Ipsum is simply dummy text of the printing and typesetting industry')) {
+    throw new Error('Missing content');
+  }
+}
